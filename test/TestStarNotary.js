352f47df4b3ea5662e30446369e56371f9953f22
Karolina Kafel <k4rolina.kafel@gmail.com>
2019-02-23 13:23:59 +0100

    Updated v2 with project 5 instructions

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -9,11 +9,9 @@ contract('StarNotary', (accs) => {
 });
 
 it('can Create a Star', async() => {
-    let instance = await StarNotary.deployed();
     let tokenId = 1;
-    
+    let instance = await StarNotary.deployed();
     await instance.createStar('Awesome Star!', tokenId, {from: accounts[0]})
-    
     assert.equal(await instance.tokenIdToStarInfo.call(tokenId), 'Awesome Star!')
 });
 
@@ -22,10 +20,8 @@ it('lets user1 put up their star for sale', async() => {
     let user1 = accounts[1];
     let starId = 2;
     let starPrice = web3.utils.toWei(".01", "ether");
-    
     await instance.createStar('awesome star', starId, {from: user1});
     await instance.putStarUpForSale(starId, starPrice, {from: user1});
-    
     assert.equal(await instance.starsForSale.call(starId), starPrice);
 });
 
@@ -75,4 +71,29 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
     const balanceAfterUser2BuysStar = await web3.eth.getBalance(user2);
     let value = Number(balanceOfUser2BeforeTransaction) - Number(balanceAfterUser2BuysStar);
     assert.equal(value, starPrice);
-  });
+});
+
+// Implement Task 2 Add supporting unit tests
+
+it('can add the star name and star symbol properly', async() => {
+    // 1. create a Star with different tokenId
+    //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
+});
+
+it('lets 2 users exchange stars', async() => {
+    // 1. create 2 Stars with different tokenId
+    // 2. Call the exchangeStars functions implemented in the Smart Contract
+    // 3. Verify that the owners changed
+});
+
+it('lets a user transfer a star', async() => {
+    // 1. create a Star with different tokenId
+    // 2. use the transferStar function implemented in the Smart Contract
+    // 3. Verify the star owner changed.
+});
+
+it('lookUptokenIdToStarInfo test', async() => {
+    // 1. create a Star with different tokenId
+    // 2. Call your method lookUptokenIdToStarInfo
+    // 3. Verify if you Star name is the same
+});
